145805489409d96bbcceaa1f000fcbc892239d53
rgchen <rgchen@neolink.com.cn>
2017-01-19 19:47:49 +0800

    添加获取手机屏幕物理尺寸，添加一个实例图片

--- app/src/main/java/com/example/crg/getphonemetricsordensitydpi/MainActivity.java
+++ app/src/main/java/com/example/crg/getphonemetricsordensitydpi/MainActivity.java
@@ -1,25 +1,47 @@
 package com.example.crg.getphonemetricsordensitydpi;
 
+import android.graphics.Point;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private int with;
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private int width;
     private int height;
     private float density;
     private float densityDpi;
-    private TextView withTextView;
+    private float phoneScreenPhysicalSize;
+    private double physicalWidth;
+    private double physicalHeight;
+
+    /**
+     * display phone screen width pixels
+     */
+    private TextView widthTextView;
+
+    /**
+     *  display phone screen height pixels
+     */
     private TextView heightTextView;
+
+    /**
+     *  display phone screen density
+     */
     private TextView densityTextView;
+
+    /**
+     *  display phone screen density dpi
+     */
     private TextView densityDpiTextView;
+
+    /**
+     *  display phone screen physical size
+     */
+    private TextView phonePhysicalSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +53,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        withTextView = (TextView) findViewById(R.id.with);
+        widthTextView = (TextView) findViewById(R.id.with);
         heightTextView = (TextView) findViewById(R.id.height);
         densityTextView = (TextView) findViewById(R.id.desity);
         densityDpiTextView = (TextView) findViewById(R.id.desitydpi);
-        withTextView.setText(with+"");
+        phonePhysicalSize = (TextView) findViewById(R.id.physical_size);
+        widthTextView.setText(width +"");
         heightTextView.setText(height+"");
         densityTextView.setText(density + "");
         densityDpiTextView.setText(densityDpi+"");
+        phonePhysicalSize.setText(phoneScreenPhysicalSize+" || " + "    宽为：" + physicalWidth + " || " + " 高为：" + physicalHeight);
     }
 
     private void getMetrics() {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        with = displayMetrics.widthPixels;
-        height = displayMetrics.heightPixels;
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getRealSize(point);
+
+        width = point.x;
+        height = point.y;
         density = displayMetrics.density;
         densityDpi = displayMetrics.densityDpi;
+
+
+        Log.d(TAG, "===> point.x: " + point.x);
+        Log.d(TAG, "===> point.y: " + point.y);
+        physicalWidth = width/displayMetrics.xdpi;
+        physicalHeight = height/displayMetrics.ydpi;
+        phoneScreenPhysicalSize = (float) Math.sqrt(Math.pow(physicalWidth, 2) + Math.pow(physicalHeight, 2));
+
     }
 
 }
